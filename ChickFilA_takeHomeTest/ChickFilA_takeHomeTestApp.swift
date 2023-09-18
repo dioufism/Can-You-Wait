e483f2ced6add4e9292cd23f08fa09f673a14d7b
Ousmane <dioufism@gmail.com>
2023-09-18 14:05:53 -0500

    Initial Commit

---
+++ ChickFilA_takeHomeTest/ChickFilA_takeHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickFilA_takeHomeTestApp.swift
+//  ChickFilA_takeHomeTest
+//
+//  Created by ousmane diouf on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ChickFilA_takeHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChickFilA_takeHomeTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChickFilA_takeHomeTest
+//
+//  Created by ousmane diouf on 9/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
